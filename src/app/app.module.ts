3f3bc3fc26c0421adb5bd33f34f258c952f73ee4
Shawn Contant <shawnc366@gmail.com>
2017-03-20 22:45:08 -0500

    Added Material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -19,6 +20,7 @@ import { DashboardComponent } from './dashboard.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
